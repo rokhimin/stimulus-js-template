9ccbb9ed4b2ee62f3020ec60ca6bc12fc850997b
rokhimin <rokhim.whd@gmail.com>
2025-05-19 18:15:53 +0700

    greeting

--- assets/js/application.js
+++ assets/js/application.js
@@ -4,9 +4,11 @@ import ThemeController from "./controllers/theme_controller.js";
 import NavbarController from "./controllers/navbar_burger_controller.js";
 import MessageController from "./controllers/message_controller.js";
 import ThumbnailController from "./controllers/thumbnail_controller.js";
+import GreetingController from "./controllers/greeting_controller.js";
 
 window.Stimulus = Application.start();
 Stimulus.register("theme", ThemeController);
 Stimulus.register("navbar", NavbarController);
 Stimulus.register("message", MessageController);
 Stimulus.register("thumbnail", ThumbnailController);
+Stimulus.register("hello", GreetingController);
